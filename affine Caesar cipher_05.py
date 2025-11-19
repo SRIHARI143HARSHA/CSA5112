e6b28ef591872a79e7406d55dfa646a22c004066
srihari-2005 <ramayanapusrihari@gmail.com>
2025-11-19 13:28:30 +0530

    Update and rename affine Caesar cipher_05 to affine Caesar cipher_05.py

--- affine Caesar cipher_05.py
+++ affine Caesar cipher_05.py
@@ -47,3 +47,15 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+
+OUTPUT:
+=== Affine Cipher ===
+Valid 'a': 1,3,5,7,9,11,15,17,19,21,23,25
+'e' for encrypt, 'd' for decrypt: e
+Enter text: srihih
+Enter a: 3
+Enter b: 5
+Encrypted: HEDADA
+
+=== Code Execution Successful ===
